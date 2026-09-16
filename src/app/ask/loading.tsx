a675f161fc3a41cc040fbae5c1cdefa620b41753
welcomedigitalbox-web <welcome.digitalbox@gmail.com>
2026-09-16 07:05:25 +0000

    restore package.json, remove wrongly uploaded CRM files

--- src/app/ask/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="space-y-4">
-      <div className="h-7 w-40 animate-pulse rounded bg-edge" />
-      <div className="card h-[70vh] animate-pulse lg:h-[calc(100vh-9rem)]" />
-    </div>
-  );
-}
